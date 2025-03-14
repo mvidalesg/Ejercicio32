9c2b5a76b6b182bca8db6eb78704d6fef65c228c
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-03-13 22:46:25 -0500

    ejercicio32

---
+++ Ejercicio32/Ejercicio32/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ejercicio32;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el tamaño del rombo: ");
+        int tamaño = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Rombo rombo = new Rombo(tamaño);
+            rombo.Dibujar();
+            rombo.Mostrar();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Ejercicio32/Ejercicio32/Rombo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio32
+{
+    class Rombo
+    {
+        private int tamaño;
+        private char[,] matriz;
+
+        public Rombo(int tamaño)
+        {
+            if (tamaño % 2 == 0)
+            {
+                throw new ArgumentException("El tamaño debe ser un número impar.");
+            }
+
+            this.tamaño = tamaño;
+            matriz = new char[tamaño, tamaño];
+
+            // Inicializar con espacios
+            for (int i = 0; i < tamaño; i++)
+            {
+                for (int j = 0; j < tamaño; j++)
+                {
+                    matriz[i, j] = ' ';
+                }
+            }
+        }
+
+        public void Dibujar()
+        {
+            int mitad = tamaño / 2;
+            for (int i = 0; i <= mitad; i++)
+            {
+                matriz[i, mitad - i] = '#';
+                matriz[i, mitad + i] = '#';
+                matriz[tamaño - i - 1, mitad - i] = '#';
+                matriz[tamaño - i - 1, mitad + i] = '#';
+            }
+        }
+
+        public void Mostrar()
+        {
+            for (int i = 0; i < tamaño; i++)
+            {
+                for (int j = 0; j < tamaño; j++)
+                {
+                    Console.Write(matriz[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
